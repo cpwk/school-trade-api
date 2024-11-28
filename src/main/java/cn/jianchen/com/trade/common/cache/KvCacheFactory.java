2ad968aea357f434235f3c745ba41a5e809cac3c
chenpeng <chenpeng396@crpcg.com>
2024-11-28 17:18:49 +0800

    缓存

--- src/main/java/cn/jianchen/com/trade/common/cache/KvCacheFactory.java
+++ src/main/java/cn/jianchen/com/trade/common/cache/KvCacheFactory.java
@@ -40,7 +40,7 @@ public class KvCacheFactory {
     public void init() {
         JedisPool redisPool = RedisUtils.createPool(config);
         executor = new RedisKvCacheExecutor(redisPool);
-        client = new RedisClient(redisPool, true);
+        //client = new RedisClient(redisPool, true);
     }
 
     public <K, T> KvCacheWrap<K, T> create(CacheOptions options, RepositoryProvider<K, T> repository,
